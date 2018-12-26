cfa91293cc8ca7610b9adf0701dbb46e36d9e151
robbyhecht <robbyhecht@gmail.com>
2018-12-25 23:40:30 -0600

    working through tutorial. Xs are rendering. Passing values from board to square-  onClick and value

--- src/index.js
+++ src/index.js
@@ -1,23 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null
+    };
+  }
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button className="square"
+      onClick={() => 
+      this.setState({ value: "X" })}>
+        {this.state.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      squares: Array(9).fill(null),
+    }
+  }
+
   renderSquare(i) {
-    return <Square />;
+    return (
+    <Square value={this.state.squares[i]} 
+    onClick={() => this.handleClick(i)}
+    />
+    )
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = "Next player: X";
 
     return (
       <div>
@@ -60,7 +80,4 @@ class Game extends React.Component {
 
 // ========================================
 
-ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
-);
+ReactDOM.render(<Game />, document.getElementById("root"));
